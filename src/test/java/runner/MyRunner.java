5f43b3d774bfce75117a12d0eabc5a10c1385367
viskrishnan <krishnansubramanian007@gmail.com>
2023-03-26 23:02:49 +0800

    Runner Class Update

--- src/test/java/runner/MyRunner.java
+++ src/test/java/runner/MyRunner.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/resources/features",
         glue = {"stepdefinitions", "hooks"},
+        dryRun = true,
+        monochrome = true,
         plugin = {"pretty","html:target/CucumberReports/CucumberReport.html"},
             publish = true)
 
